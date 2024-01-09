5aa310f12c427413d57e7e962e2feb7f6edf5820
Anshugupta08 <gomtinagar08@gmail.com>
2024-01-10 00:02:18 +0530

    Commit

---
+++ src/login.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class MyForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>heelo bhaiya</h1>
+      </div>
+    );
+  }
+}
+
+export default MyForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 // import MyForm from "./Data1";
-import ClassComponentForm from "./Data3.jsx";
+// import ClassComponentForm from "./Data3.jsx";
 // import App from "./App.jsx";
 // import App from './App.jsx'
 // import './index.css'
 // import LoginData from "./Data.jsx";
+import MyForm from "./login.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ClassComponentForm />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<MyForm />);
